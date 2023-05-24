2760dd691e0d58f131924d9d0cf24efe98337340
PaulaRudzka <paulalendorka@gmail.com>
2023-05-24 20:50:36 +0200

    K

--- K_fit.c
+++ K_fit.c
@@ -1,33 +1,31 @@
-//Paulina Rudzka i Jakub Jaroszewski
-//sieci komputerowe 2023
-//kod bazujący na programie wykonania: W.Bajdecki 
+//Paulina Rudzka Jakub Jaroszewski
+//SK 2023
+//Program bazuje na programie W. Bajdeckiego
 
-
-#include <sys/types.h> //typy danych
-#include <sys/socket.h> //gniazda
-#include <netinet/in.h> //adresy sieciowe
-#include <arpa/inet.h> //adresy sieciowe
-#include <netdb.h> //obsluga hostow
-#include <stdio.h> //wejscie/wyjscie std
-#include <stdlib.h> //ogólne 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h> /* close */
+#include <string.h> /* memset() */
 
-int main (int argc, char *argv[]) { /* licznik argumentow, tablica argumentow */
 
-//inicjalizuje stale
+int main (int argc, char *argv[]) { //licznik argumentów, tablica argumentów
+
   const int SERVER_PORT=1500;
   int sd, rc;
   struct sockaddr_in localAddr, servAddr;
   struct hostent *h;
   
-//zabezpieczenie przed złym wywołaniem
   if(argc < 2) {
     printf("usage: %s <server>\n",argv[0]);
     exit(1);
   }
 
-//zmiana nazwy domenowej na adres IP maszyny
-  h = gethostbyname(argv[1]); 
+  h = gethostbyname(argv[1]); //zamiana nazwy domenowej na adres IP maszyny
   if(h==NULL) {
     printf("%s: unknown host '%s'\n",argv[0],argv[1]);
     exit(1);
@@ -44,47 +42,54 @@ int main (int argc, char *argv[]) { /* licznik argumentow, tablica argumentow */
     exit(1);
   }
 
- //łączenie z serwerem
+//łączenie z serwerem
   rc = connect(sd, (struct sockaddr *) &servAddr, sizeof(servAddr));
   if(rc<0) {
     perror("cannot connect ");
     exit(1);
   }
- 
- //powitanie nowego gracza
+
+  char bufor[500];
+  char bufor1[500];
+//powitanie
   printf("Gra zgadywanie słów. Wpisz literę, by sprawdzić czy jest w haśle.  (wpisz znak / aby wyjść, HELP by wywołać pomoc):\n");
   
-
   while (1){
-
-
-//wczytanie znaku od klienta
 	  fgets(bufor, 500, stdin);
 	  
-//koniec jeżeli /
+//rozłączenie po wciśnięciu /
 	  if (bufor[0] == '/')
 		break;
-
-//wysłanie litery/hasła do serwera
+	  
+     
+//wysłanie danych do serwera
 	  rc = send(sd, bufor, strlen(bufor)+1, 0);
+	  
 	  if(rc<0) {
 	    perror("cannot send data ");
 	    close(sd);
 	    exit(1);
 	  }
+
+
+    strcpy(bufor1, bufor); // do zmiennej bufor1 zapisujemy litere 
+    //zapisac bufor do innej zmiennej
+	  memset(bufor, 0, sizeof(bufor));  // wyzerowanie bufora, aby nie wysyłać poprzedniej wiadomości
   
-//odpowiedź serwera
-	  rc = recv(sd, bufor, sizeof(bufor), 0); 
+	  rc = recv(sd, bufor, sizeof(bufor), 0); //otrzymuemy nasze haslo do zgadniecia 
+
+  
+
 	  if(rc<0) {
 	    perror("cannot receive data ");
 	    close(sd);
 	    exit(1);
 	  }
+
 	  printf("Odpowiedź serwera: %s\n", bufor);
 	  memset(bufor, 0, sizeof(bufor)); // wyzerowanie bufora
   }
 
-//zakończenie
   close(sd);
   return 0;
   
